316dd7cc53e85f9de4d21fc589ddaea9c703cd70
hrodberaht <robpszczolka@gmail.com>
2018-09-17 21:36:47 +0200

    add some inline styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,11 +5,26 @@ export default class Card extends Component {
     const { name, email, id} = this.props.card;
     const {handleClick} = this.props;
     return (
-      <div>
+      <div style={this.card}>
         <h2>Name: {name} </h2>
         <h4>Email: {email}</h4>
-        <button type={"button"} onClick={() => handleClick(id)}>Delete</button>
+        <button style={this.button} type={"button"} onClick={() => handleClick(id)}>Delete</button>
       </div>
     );
+  }
+
+  card = {
+      border: "1px solid black",
+      width: "300px",
+      margin: "5px auto"
+  }
+
+  button = {
+      color: "white",
+      border: "1px solid red",
+      fontSize: "16px",
+      backgroundColor: "red",
+      padding: "5px"
+
   }
 }
