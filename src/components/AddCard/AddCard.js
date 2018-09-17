3764a41538958927463e4d97f1a7e834977f427f
hrodberaht <robpszczolka@gmail.com>
2018-09-17 19:28:11 +0200

    add soem styling to addCard

--- src/components/AddCard/AddCard.js
+++ src/components/AddCard/AddCard.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
+import "./AddCard.css";
 
 export default class AddCard extends Component {
   render() {
-    const {handleClick, showAddCard} = this.props; 
-    const form = 
-    (<div>
+    const { handleClick, showAddCard } = this.props;
+    const form = (
+      <div className={"AddCard"}>
         <label htmlFor={"name"}>Name: </label>
         <input id={"name"} type={"text"} />
         <label htmlFor={"email"}>Email: </label>
         <input id={"email"} type={"email"} />
         <button type={"button"}>Add</button>
-    </div>);
+      </div>
+    );
 
     return (
-        <div>
-            <button type={"button"} onClick={handleClick}>Add Card</button>
-            {showAddCard ? form : null}
-        </div>
+      <div>
+        <button type={"button"} onClick={handleClick}>
+          {showAddCard ? "Hide" : "Add Card"}
+        </button>
+        {showAddCard ? form : null}
+      </div>
     );
   }
 }
