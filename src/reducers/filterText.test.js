acdcb09d58ab6f229f9aef6f81e590522d861022
hrodberaht <robpszczolka@gmail.com>
2018-09-22 21:29:44 +0200

    add test to reducers

---
+++ src/reducers/filterText.test.js
@@ -0,0 +1,16 @@
+import filterText from "./filterText";
+
+describe("filterText reducer", () => {
+  it("should have intial state set to empty string", () => {
+    expect(filterText(undefined, {})).toEqual("");
+  });
+
+  it("should filter text equal to string passed by action", () => {
+    expect(
+      filterText("", {
+        type: "FILTER",
+        filterText: "foo"
+      })
+    ).toEqual("foo");
+  });
+});
