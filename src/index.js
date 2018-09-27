5179ebca2e3a9b66a2b3e3abbcf5d1e4dbc6ecdb
hrodberaht <robpszczolka@gmail.com>
2018-09-27 13:06:32 +0200

    install redux devtools

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 import App from './App';
 
-
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+
+const middleware = [];
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
+
 
 const AppWithProvider = () => (
   <Provider store={store}>
