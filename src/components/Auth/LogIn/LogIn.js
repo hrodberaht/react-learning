deae04bc19d2c7022b3d5326d5d1cbf9e1993e78
hrodberaht <robpszczolka@gmail.com>
2018-10-01 18:36:01 +0200

    add login form

--- src/components/Auth/LogIn/LogIn.js
+++ src/components/Auth/LogIn/LogIn.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class LogIn extends Component {
-  render() {
-    return (
-      <div>
-        <input />
-        <input />
-        <button type="submit">Log In</button>
-      </div>
-    );
-  }
+    state = {
+      login: '',
+      email: '',
+    }
+
+    handleChange = (e) => {
+      this.setState({ [e.target.id]: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+
+      this.setState({
+        login: '',
+        email: '',
+      });
+    }
+
+    render() {
+      const { login, email } = this.state;
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input id="login" value={login} onChange={this.handleChange} />
+          <input id="email" value={email} onChange={this.handleChange} />
+          <button type="submit">Log In</button>
+        </form>
+      );
+    }
 }
+
+LogIn.propTypes = {
+
+};
