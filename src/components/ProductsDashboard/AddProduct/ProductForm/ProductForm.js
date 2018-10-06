0c72c25dbf3f11c43dc7efcec50191956cfcf610
hrodberaht <robpszczolka@gmail.com>
2018-10-06 17:57:12 +0200

    add simple validation

--- src/components/ProductsDashboard/AddProduct/ProductForm/ProductForm.js
+++ src/components/ProductsDashboard/AddProduct/ProductForm/ProductForm.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { PropTypes } from 'prop-types';
 
+const validate = (values) => {
+  const errors = {};
+  if (!values.name) {
+    errors.name = 'Required';
+  } else if (values.name.length < 3) {
+    errors.name = 'Name should have at least 3 letters';
+  }
+
+  if (!values.price) {
+    errors.price = 'Required';
+  } else if (values.price <= 0) {
+    errors.price = 'Price should be bigger than 0';
+  }
+
+  return errors;
+};
+
+const renderField = ({
+  input,
+  type,
+  meta: { touched, error },
+}) => (
+  <p>
+    <input {...input} type={type} />
+    {touched
+        && (error && <span>{error}</span>)}
+  </p>
+);
+
 class ProductForm extends Component {
   render() {
     const { handleSubmit } = this.props;
@@ -10,11 +39,11 @@ class ProductForm extends Component {
         <form onSubmit={handleSubmit}>
           <label htmlFor="name">
             Product Name:
-            <Field id="name" name="name" component="input" type="text" />
+            <Field id="name" name="name" component={renderField} type="text" />
           </label>
           <label htmlFor="price">
             Price:
-            <Field id="price" name="price" component="input" type="number" />
+            <Field id="price" name="price" component={renderField} type="number" />
           </label>
           <button type="submit">Add</button>
         </form>
@@ -25,6 +54,7 @@ class ProductForm extends Component {
 
 export default reduxForm({
   form: 'addProduct',
+  validate,
 })(ProductForm);
 
 ProductForm.propTypes = {
