77a302ff0150162ee49d999d687942b5ff9677db
hrodberaht <robpszczolka@gmail.com>
2018-10-15 19:32:43 +0200

    validation and async validation

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+const validate = (values) => {
+  const errors = {};
+  if (!values.login) {
+    errors.login = 'Required';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  }
+  return errors;
+};
+
+const asyncValidate = values => Promise.resolve().then(() => {
+  // simulate server latency
+  if (['rob'].includes(values.login)) {
+    throw { login: 'That username is taken' };
+  }
+});
+
+
+const renderInput = ({
+  input,
+  type,
+  placeholder,
+  meta: {
+    touched,
+    error,
+  },
+}) => (
+  <p>
+    <input {...input} type={type} placeholder={placeholder} />
+    {touched && error && <span>{ error }</span>}
+  </p>
+);
 export class Registration extends Component {
 submit = (values) => {
   console.log(values);
 }
+
 
 render() {
   const { handleSubmit } = this.props;
@@ -14,23 +51,24 @@ render() {
         <Field
           id="login"
           name="login"
-          component="input"
+          label="login"
           placeholder="Login"
           type="text"
+          component={renderInput}
         />
         <Field
           id="email"
           name="email"
-          component="input"
           placeholder="Email"
           type="email"
+          component={renderInput}
         />
         <Field
           id="password"
           name="password"
-          component="input"
           placeholder="Password"
           type="password"
+          component={renderInput}
         />
         <button type="submit" className="btn submit-btn">Submit</button>
       </form>
@@ -41,4 +79,7 @@ render() {
 
 export default reduxForm({
   form: 'registration',
+  validate,
+  asyncValidate,
+  asyncChangeFields: ['login'],
 })(Registration);
