1eb8e11b8d1e28402b21846fcdb0d1338df191a0
hrodberaht <robpszczolka@gmail.com>
2018-10-18 18:06:45 +0200

    add hamburger menu

--- src/components/Header/NavBar/NavBar.js
+++ src/components/Header/NavBar/NavBar.js
@@ -6,24 +6,48 @@ import ConnectedAuthorization from './Authorization/Authorization';
 export default class NavBar extends Component {
   render() {
     return (
-      <nav className="NavBar">
-        <ul>
-          <li>
-            <NavLink to="/" exact>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cards">Cards</NavLink>
-          </li>
-          <li>
-            <NavLink to="/products">Products</NavLink>
-          </li>
-          <li>
-            <ConnectedAuthorization
-              withAuth={logOut => <NavLink to="/login" onClick={logOut}>Log out</NavLink>}
-              withoutAuth={<NavLink to="/login">Login</NavLink>}
-            />
-          </li>
-        </ul>
+      <nav>
+        <div className="NavBar">
+          <ul>
+            <li>
+              <NavLink to="/" exact>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cards">Cards</NavLink>
+            </li>
+            <li>
+              <NavLink to="/products">Products</NavLink>
+            </li>
+            <li>
+              <ConnectedAuthorization
+                withAuth={logOut => <NavLink to="/login" onClick={logOut}>Log out</NavLink>}
+                withoutAuth={<NavLink to="/login">Login</NavLink>}
+              />
+            </li>
+          </ul>
+        </div>
+        <div className="NavBarNarrow">
+          <i className="fa fa-bars fa-2x" />
+          <div className="menu">
+            <ul>
+              <li>
+                <NavLink to="/" exact>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/cards">Cards</NavLink>
+              </li>
+              <li>
+                <NavLink to="/products">Products</NavLink>
+              </li>
+              <li>
+                <ConnectedAuthorization
+                  withAuth={logOut => <NavLink to="/login" onClick={logOut}>Log out</NavLink>}
+                  withoutAuth={<NavLink to="/login">Login</NavLink>}
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     );
   }
